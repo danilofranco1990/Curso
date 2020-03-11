c9662f4cf21aaf5642b12853db490e7483d76cd9
Danilo Franco <danilo.franco90@gmail.com>
2020-03-10 22:21:41 -0300

    Curso Inicio

---
+++ src/test/java/com/projeto/curso/CurdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
